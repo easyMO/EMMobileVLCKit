c05836efa06d75d4606fa1a6caaccca55737ae88
Philipp Wallisch <philipp.wallisch@inode.at>
2019-06-25 23:13:37 +0200

    Release under LGPLv2.1 license

--- EMMobileVLCKit/EMVLCMedia.m
+++ EMMobileVLCKit/EMVLCMedia.m
@@ -2,8 +2,21 @@
 //  EMVLCMedia.m
 //  EMMobileVLCKit
 //
-//  Created by Philipp Wallisch on 25.06.19.
-//  Copyright © 2019 easyMOBIZ mobile IT solutions GmbH. All rights reserved.
+//  Copyright (C) 2019 easyMOBIZ mobile IT solutions GmbH
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
 #import "EMVLCMedia+Protected.h"
